c5c3f759c550c541f93f6cbca8566ea85eb3acbe
SohailMG <72792684+SohailMG@users.noreply.github.com>
2020-10-18 17:23:32 +0100

    fixed file name

---
+++ Leaderboard.php
@@ -0,0 +1,13 @@
+<?php
+    include('Common.php');
+    outputhtmlTemp("Leaderboard");
+    outputBannerNavigation("Leaderboard")
+
+?>
+
+
+<?php
+    include('Common.php');
+    outputfooter();
+
+?>
