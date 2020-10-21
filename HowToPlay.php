e2d72da462b279a33f661819f2a494a2c599b390
SohailMG <72792684+SohailMG@users.noreply.github.com>
2020-10-21 18:16:35 +0100

    added grid layout to how to play page

--- HowToPlay.php
+++ HowToPlay.php
@@ -4,6 +4,10 @@
     outputBannerNavigation("HowToPlay")
 
 ?>
+    <div class="grid-container">
+        <div class="grid-item grid-item-1"></div>
+        <div class="grid-item grid-item-2"></div>
+    </div>
 
 
 <?php
