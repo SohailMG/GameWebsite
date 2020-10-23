5809b6e108e2907547017f092022e686770731c9
SohailMG <72792684+SohailMG@users.noreply.github.com>
2020-10-23 13:09:36 +0100

    added score table in leaderbaord page

--- Leaderboard.php
+++ Leaderboard.php
@@ -4,6 +4,55 @@
     outputBannerNavigation("Leaderboard")
 
 ?>
+    <div class="grid-container2">
+        <div class="grid-item score-table">
+        <table class="table-container">
+                
+            <thead> 
+                <tr>
+                    <th>Place</th>
+                    <th>Username</th>
+                    <th>Level</th>
+                    <th>score</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th>3</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th>4</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th>5</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+        <div class="grid-item highest-score">highest score</div>
+        <div class="grid-item user-name">user name</div>
+    </div>
 
 
 <?php
