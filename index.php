49000ea57a46baffa2bfaafee51db2cf40ac88e2
SohailMG <72792684+SohailMG@users.noreply.github.com>
2020-10-24 17:47:39 +0100

    added logo to homepage

--- index.php
+++ index.php
@@ -3,23 +3,24 @@
         outputhtmlTemp("Home Page");
         outputBannerNavigation("Home")
 ?>
-        <div class="signbtns">
-            <p><b>Are you registered?</b></p>
-            <a  class="current" href="Sign-in.php"><b>Sign-in</b></a>
-            <a  href="Sign-up.php"><b>Sign-up</b></a>
-        </div>        
-    <main>
-            <div class="box1">
-                <h1>Welcome to Memory Master</h1> 
-                <div class="questions">           
-                <ul>How good is your visual memory?</ul>
-                <ul>How many numbers can you remember in a short amount of time?</ul>
-                <ul> Test your memory strength and become the Memory Master</ul>
-                </div>
-            </div>
-            <div class="box2">
-            </div>
-    </main>
+<div class="signbtns">
+    <p><b>Are you registered?</b></p>
+    <a class="current" href="Sign-in.php"><b>Sign-in</b></a>
+    <a href="Sign-up.php"><b>Sign-up</b></a>
+</div>
+<main>
+    <div class="box1">
+        <h1>Welcome to Memory Master</h1>
+        <div class="questions">
+            <ul>How good is your visual memory?</ul>
+            <ul>How many numbers can you remember in a short amount of time?</ul>
+            <ul> Test your memory strength and become the Memory Master</ul>
+        </div>
+    </div>
+    <div class="box2">
+        <img src="Images/Master.png" alt="">
+    </div>
+</main>
 <?php
     outputfooter();
 ?>
